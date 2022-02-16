9e7891d0490c477dc8c0675bf880b9e0c3fb15d5
MD Jannatin Naim <mail.jnaim@gmail.com>
2022-02-16 22:13:22 +0600

    Class Seventeen

---
+++ Class Seventeen/hello.js
@@ -0,0 +1,33 @@
+console.log("Hello, world!");
+
+// var, let
+// const
+
+// String
+// Number
+
+// A-Z, a-z, 0-9, _, $,
+
+/*
+    1. Variables
+    2. Data Types
+    3. Operators
+    4. Functions
+    5. Arrays
+    6. Objects
+    7. Classes
+    8. Modules
+    9. Loops
+    10. Data Structure
+*/
+
+var text = "Hello, world!";
+text = "Hello, JavaScript!";
+
+var num = 69;
+
+console.log(text);
+console.log(num);
+
+const PI = 3.1416;
+console.log(PI);

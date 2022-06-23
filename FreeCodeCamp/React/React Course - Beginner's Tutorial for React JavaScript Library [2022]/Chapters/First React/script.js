d9163b8cd19ce6b801857e5216f7df807dcffaae
Jannatin Naim <contact.jannatinnaim@gmail.com>
2022-06-23 22:29:47 +0600

    basic react v2

--- FreeCodeCamp/React/React Course - Beginner's Tutorial for React JavaScript Library [2022]/Chapters/First React/script.js
+++ FreeCodeCamp/React/React Course - Beginner's Tutorial for React JavaScript Library [2022]/Chapters/First React/script.js
@@ -1,24 +1,15 @@
-function CustomComponent() {
-  return (
-    <div>
-      <h2>A Custom Component</h2>
-      <p>Text from a custom component.</p>
-    </div>
-  );
-}
+const page = (
+  <nav>
+    <h1>Jannatin Naim</h1>
 
-ReactDOM.render(
-  <div>
-    <h1>Soup</h1>
-    <CustomComponent />
-  </div>,
-  document.getElementById("root")
+    <ul>
+      <li>Pricing</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
 );
 
 const rootElement = document.getElementById("root");
 
-const h1Element = document.createElement("h1");
-h1Element.textContent = "Soup";
-h1Element.classList.add("header");
-
-rootElement.append(h1Element);
+ReactDOM.render(page, root);

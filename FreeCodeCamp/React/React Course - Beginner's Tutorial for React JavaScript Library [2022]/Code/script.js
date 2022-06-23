53f59804f3ad3f23a84788f51b59d5d2b6f28466
Jannatin Naim <contact.jannatinnaim@gmail.com>
2022-06-23 23:16:35 +0600

    first project?

--- FreeCodeCamp/React/React Course - Beginner's Tutorial for React JavaScript Library [2022]/Code/script.js
+++ FreeCodeCamp/React/React Course - Beginner's Tutorial for React JavaScript Library [2022]/Code/script.js
@@ -1,20 +1,44 @@
+function Header() {
+  return (
+    <header className="header">
+      <nav className="nav">
+        <img className="nav-logo" src="./react.png" alt="React logo" />
+
+        <ul className="nav-items">
+          <li className="nav-item">Pricing</li>
+          <li className="nav-item">About</li>
+          <li className="nav-item">Contact</li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+function Footer() {
+  return <footer className="footer">&copy; 2022 Jannatin Naim. All rights reserved.</footer>;
+}
+
+function Main() {
+  return (
+    <main className="main">
+      <h1 className="title">Why I'm Learning React</h1>
+      <ol className="content-list">
+        <li className="content-item">IDK, seems popular I guess...</li>
+        <li className="content-item">I wanna get paid soon... maybe?</li>
+        <li className="content-item">Don't have anything better to do... sort of.</li>
+      </ol>
+    </main>
+  );
+}
+
 function Page() {
   return (
     <div>
-      <header>
-        <nav>
-          <img src="./react.png" width="100" alt="React logo" />
-        </nav>
-      </header>
+      <Header />
 
-      <h1>Why I'm Learning React</h1>
-      <ol>
-        <li>IDK, seems popular I guess...</li>
-        <li>I wanna get paid soon... maybe?</li>
-        <li>Don't have anything better to do... sort of.</li>
-      </ol>
+      <Main />
 
-      <footer>&copy; 2022 Jannatin Naim. All rights reserved.</footer>
+      <Footer />
     </div>
   );
 }

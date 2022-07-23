14b6c64a21d061d149696be10c41bce1dc589c5a
Jannatin Naim <contact.jannatinnaim@gmail.com>
2022-07-24 03:06:19 +0600

    reset

--- FreeCodeCamp/React/React Course - Beginner's Tutorial for React JavaScript Library [2022]/Code/react-app/src/index.js
+++
@@ -1,9 +0,0 @@
-import "./styles/index.scss";
-
-import App from "./pages";
-import ReactDOM from "react-dom/client";
-
-const HTMLRoot = document.getElementById("root");
-const ReactRoot = ReactDOM.createRoot(HTMLRoot);
-
-ReactRoot.render(<App />);

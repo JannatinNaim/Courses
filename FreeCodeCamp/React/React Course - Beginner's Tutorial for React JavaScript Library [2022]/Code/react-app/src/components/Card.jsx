67e28e9464bb050d8c76e9ae28bd33c94a25c558
Jannatin Naim <contact.jannatinnaim@gmail.com>
2022-07-24 03:22:39 +0600

    airbnb clone? nah

--- FreeCodeCamp/React/React Course - Beginner's Tutorial for React JavaScript Library [2022]/Code/react-app/src/components/Card.jsx
+++ FreeCodeCamp/React/React Course - Beginner's Tutorial for React JavaScript Library [2022]/Code/react-app/src/components/Card.jsx
@@ -1,31 +1,36 @@
-import Banner from "../images/item_b.png";
+const Card = (props) => {
+  const { image, status, rating, reviews, country, description, price } = props;
 
-const Card = () => {
   return (
     <div className="card">
       <div className="card-banner">
-        <img className="banner-image" src={Banner} alt="Item description." />
-        <span className="status">sold</span>
+        <img
+          className="banner-image"
+          src={"./images/" + image}
+          alt="Item description."
+        />
+        <span className="status">{status.toUpperCase()}</span>
       </div>
 
       <div className="card-stats">
         <span className="rating">
-          <span className="rating-icon">X</span> 4.8
+          <span className="rating-icon">X</span>
+          <span className="rating-text">{rating}</span>
         </span>
 
         <span className="divider"> </span>
 
-        <span className="reviews">(5)</span>
+        <span className="reviews">({reviews})</span>
 
         <span className="divider"> . </span>
 
-        <span className="country">USA</span>
+        <span className="country">{country.toUpperCase()}</span>
       </div>
 
-      <p className="card-description">Life lessons from some rando.</p>
+      <p className="card-description">{description}</p>
 
       <span className="card-pricing">
-        <span className="pricing">From $111</span> / person
+        <span className="pricing">From ${price}</span> / person
       </span>
     </div>
   );
--- FreeCodeCamp/React/React Course - Beginner's Tutorial for React JavaScript Library [2022]/Code/react-app/src/components/Cards.jsx
+++ FreeCodeCamp/React/React Course - Beginner's Tutorial for React JavaScript Library [2022]/Code/react-app/src/components/Cards.jsx
@@ -3,10 +3,45 @@ import Card from "./Card";
 const Cards = () => {
   return (
     <div className="cards-gallery">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <Card
+        image="item_a.png"
+        status="sold"
+        rating={4.2}
+        reviews={66}
+        country="BD"
+        description="Some description of the service."
+        price={111}
+      />
+
+      <Card
+        image="item_b.png"
+        status="available"
+        rating={4.2}
+        reviews={66}
+        country="BD"
+        description="Some description of the service."
+        price={111}
+      />
+
+      <Card
+        image="item_a.png"
+        status="sold"
+        rating={4.2}
+        reviews={66}
+        country="BD"
+        description="Some description of the service."
+        price={111}
+      />
+
+      <Card
+        image="item_b.png"
+        status="sold"
+        rating={4.2}
+        reviews={66}
+        country="BD"
+        description="Some description of the service."
+        price={111}
+      />
     </div>
   );
 };

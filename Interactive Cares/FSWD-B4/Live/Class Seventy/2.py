3d8929362af91e2217f145ae06472c3c43b98543
Jannatin Naim <contact.jannatinnaim@gmail.com>
2022-08-02 14:29:00 +0600

    fix class seventy code

--- Interactive Cares/FSWD-B4/Live/Class Seventy/2.py
+++ Interactive Cares/FSWD-B4/Live/Class Seventy/2.py
@@ -1,16 +1,11 @@
 """Fibonacci Sum Up To N"""
 n = 5
 
-numbers = [1, 1]
+numbers = [0, 1]
 
-while True:
+for i in range(n - 2):
     x = numbers[-1]
     y = numbers[-2]
-
-    if x == n:
-        numbers.pop()
-        break
-
     numbers.append(x + y)
 
 print(sum(numbers))
